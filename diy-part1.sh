4e2cd629fb82f73be6751e1da84c19f03a1acd85
Cumin <cucucumin@gmail.com>
2022-02-24 13:48:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,4 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
