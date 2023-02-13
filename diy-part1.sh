b7c45c734752d8b2487573727ebca1b33bd2e22a
Cumin <cucucumin@gmail.com>
2023-02-13 14:12:24 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #==================================================
-# Modify default IP
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
