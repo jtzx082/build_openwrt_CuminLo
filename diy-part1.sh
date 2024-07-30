f9660b2704cfa286f9dcb515dfa61325f40fe867
Cumin <4970189+CuminLo@users.noreply.github.com>
2024-07-30 17:03:27 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,6 +6,7 @@
 # Blog: https://p3terx.com
 #==================================================
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
