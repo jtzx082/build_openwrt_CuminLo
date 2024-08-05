cab137a4f6f2b0433dc8a405b21c2b5c8c82f4e1
Cumin <4970189+CuminLo@users.noreply.github.com>
2024-08-05 14:09:59 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,10 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-git clone https://github.com/gSpotx2f/luci-app-cpu-status package/app_cpu_status
-git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/app_cpu_perf
+#git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+#git clone https://github.com/gSpotx2f/luci-app-cpu-status package/app_cpu_status
+#git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/app_cpu_perf
 
-#sed -i '$a src-git app_cpu_status https://github.com/gSpotx2f/luci-app-cpu-status' feeds.conf.default
-#sed -i '$a src-git app_cpu_perf https://github.com/gSpotx2f/luci-app-cpu-perf' feeds.conf.default
+sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' feeds.conf.default
+sed -i '$a src-git app_cpu_status https://github.com/gSpotx2f/luci-app-cpu-status' feeds.conf.default
+sed -i '$a src-git app_cpu_perf https://github.com/gSpotx2f/luci-app-cpu-perf' feeds.conf.default
