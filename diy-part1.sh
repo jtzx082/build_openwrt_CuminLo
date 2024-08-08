2cf63236431d66f90e596927fbf043052f7262f2
Cumin <4970189+CuminLo@users.noreply.github.com>
2024-08-08 11:54:39 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
-
+sed -i '4i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
