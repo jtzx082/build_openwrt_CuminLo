fd02237631fb7ce9b94fe1012f09f69e6138ccf0
Cumin <4970189+CuminLo@users.noreply.github.com>
2024-08-11 16:32:16 +0800

    update

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -6,9 +6,9 @@
 # Blog: https://p3terx.com
 #==================================================
 
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
 
 #sed -i '4i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
