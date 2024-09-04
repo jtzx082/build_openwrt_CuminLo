4e666eee135e483c60faf0fb3b195bf53d98e35f
NewLuo <4970189+CuminLo@users.noreply.github.com>
2024-09-04 13:16:25 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,7 +29,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 
 # 更新; 每次获取最新 adguardhome 二进制
 update_adguardhome() {
-    adguardhome_version=$(curl -s "https://api.github.com/repos/AdguardTeam/AdGuardHome/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | awk -F "v" '{print $2}')
+    adguardhome_version="0.108.0-b.57"
+    #adguardhome_version=$(curl -s "https://api.github.com/repos/AdguardTeam/AdGuardHome/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | awk -F "v" '{print $2}')
     sed -ri "s/(PKG_VERSION:=)[^\"]*/\1$adguardhome_version/" feeds/packages/net/adguardhome/Makefile
     sed -i 's/release/beta/g' feeds/packages/net/adguardhome/Makefile
     sed -i 's/.*PKG_MIRROR_HASH.*/#&/' feeds/packages/net/adguardhome/Makefile
