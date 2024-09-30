0b274776350618e0d4033a948f4c8e57c23f8a9b
NewLuo <4970189+CuminLo@users.noreply.github.com>
2024-09-30 20:56:52 +0800

    update

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -8,4 +8,4 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+#sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
