50bb9338c481d649b8faa2586be5f06dd976e7cb
NewLuo <4970189+CuminLo@users.noreply.github.com>
2025-02-12 17:47:43 +0800

    还原

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -8,5 +8,5 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' feeds.conf.default
+# sed -i '3i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' feeds.conf.default
 #sed -i '3i src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
